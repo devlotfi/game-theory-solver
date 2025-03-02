39ee5a4502d912b3cf17747edd04c379cd9a6287
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2025-03-02 15:37:46 +0100

    init

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig, normalizePath } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const PYODIDE_EXCLUDE = [
+  "!**/*.{md,html}",
+  "!**/*.d.ts",
+  "!**/*.whl",
+  "!**/node_modules",
+];
+
+export function viteStaticCopyPyodide() {
+  const pyodideDir = dirname(fileURLToPath(import.meta.resolve("pyodide")));
+  return viteStaticCopy({
+    targets: [
+      {
+        src: [normalizePath(join(pyodideDir, "*"))].concat(PYODIDE_EXCLUDE),
+        dest: "assets",
+      },
+    ],
+  });
+}
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [viteStaticCopyPyodide(), react()],
+  optimizeDeps: { exclude: ["pyodide"] },
+  base: "/game-theory-solver/",
+  build: {
+    outDir: "./docs",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("@heroui")) {
+            return "heroui";
+          }
+        },
+      },
+    },
+  },
+});
